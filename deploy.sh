400f50be74d23f083e187ca552ed3b99225eb996
rjrrk <167875471+rjrrk@users.noreply.github.com>
2024-08-07 18:31:05 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-DOCKER_USERNAME="rjrrk"
-DOCKER_PASSWORD="Sithara@2020"
-DOCKER_REPO="prod"
+DOCKER_REPO="rjrrk/prod"
 DOCKER_TAG="latest"
 
 echo "Logging in to Docker Hub..."
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker login
 
 LOCAL_IMAGE="react-app-name"
 
 echo "Tagging the Docker image..."
-docker tag "${LOCAL_IMAGE}" "$DOCKER_USERNAME/${DOCKER_REPO}:${DOCKER_TAG}"
+docker tag "${LOCAL_IMAGE}" "${DOCKER_REPO}:${DOCKER_TAG}"
 
 
 echo "Pushing the Docker image to Docker Hub..."
-docker push "$DOCKER_USERNAME/${DOCKER_REPO}:${DOCKER_TAG}"
+docker push "${DOCKER_REPO}:${DOCKER_TAG}"
 
 echo "Deployment to Docker Hub completed successfully."
 
